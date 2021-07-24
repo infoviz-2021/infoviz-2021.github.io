2bea3f5daadd8cdef3a8824ef362d7a922021ac1
Elizabeth Duane <duanecosta@gmail.com>
2021-07-23 21:54:55 -0300

    filter in table of vis09

--- data/keys2.js
+++ data/keys2.js
@@ -37,3 +37,77 @@ var impacts = [
         "order":4
     }
   ]
+
+var challenges = [
+
+    {
+        "challenge_type":"IH",
+        "name":"HInt impacts on humans", 
+        "challenge" : "To explore the Human-Computer Integration (HInt) phenomena and effects on the individual's identity, perception and behavior"
+    },
+    {
+        "challenge_type":"IH",
+        "name":"HInt impacts on humans", 
+        'challenge': 'To explore the Human-Computer Integration (HInt) phenomena and effects  on culture, ethics, security, privacy and on the values and social aspects of each individual and society '
+
+    },
+    {
+        "challenge_type":"IH",
+        "name":"HInt impacts on humans", 
+        'challenge': 'To explore the phenomena and effects of Human-Computer Integration  (HInt) by fusion on internal and external bodily processes of individual'
+   
+    },
+    {
+        "challenge_type":"IH",
+        "name":"HInt impacts on humans", 
+        'challenge': 'To explore the Human-Computer Integration (HInt) phenomena and effects on whole integrated environment'   
+    },
+    {
+        "challenge_type":"IH",
+        "name":"HInt impacts on humans", 
+        'challenge': 'To explore the phenomena, effects and usefulness of Human-Computer Integration (HInt) in the different domains and contexts of use '
+    },
+    {
+        "challenge_type":"IH",
+        "name":"HInt impacts on humans", 
+        'challenge': 'To explore the Human-Computer Integration (HInt) effects on Universal Access and Accessibility'   
+    },
+    {
+        "challenge_type":"DC",
+        "name":"Design challenges",
+        'challenge': 'To design partner technologies compatible with the human body'   
+    },
+    {
+        "challenge_type":"DC",
+        "name":"Design challenges",
+        'challenge': 'To design partner technologies and interactions that promote symbiotic relationships and benefits for users'   
+    },
+    {
+        "challenge_type":"EC",
+        "name":"Evaluation challenges",
+        'challenge': 'To evaluate partner technologies compatible with the human body' 
+    },
+    {
+        "challenge_type":"EC",
+        "name":"Evaluation challenges",
+        "challenge": "To evaluate and to investigate users' experience with integrated technologies and interactions"   
+    },
+    {
+        "challenge_type":"DT",
+        "name":"Demands for new theories and methods",
+        'challenge': "To expand theoretical and empirical approaches to HCI to focus both human-computer interaction and integration"
+    
+    },
+    {
+        "challenge_type":"DT",
+        "name":"Demands for new theories and methods",
+        'challenge': 'To reformulate the HCI curriculum to include the Human-Computer Integration (HInt) as a new paradigm and to expand its interdisciplinarity with other areas such as Artificial Intelligence (AI) and Internet of Things (IoT)'   
+    }      
+        
+]
+
+
+
+
+
+
